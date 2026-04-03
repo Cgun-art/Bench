10bfc8f6e75cc3fdbfb4328707fcbc70cb10a857
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-04-03 14:14:26 -0300

    Create assets.UseAllFiles.lua

---
+++ Assets/assets.UseAllFiles.lua
@@ -0,0 +1,8 @@
+IF FILE.LOADED = COUNT.ASSET(EVEN.OUTSIDEASSETSFOLDER) local /Assets/ NDK = assert(0) SDK = assert(1) render(2) ASCII(3)
+file \Assets\/Assets/ * qtk * DLSS(false) * = /File.render.Asset/ = * ResultIntoSIFile.
+
+return(854+ Asset/854asOptionalData/ \ * Folder * 0123456789Y \ /dat.T.Asset/ * Asset = SI;
+
+CreateAssetAs(SI).
+
+}
