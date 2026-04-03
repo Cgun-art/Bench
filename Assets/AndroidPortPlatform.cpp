a827e272bfefe421959f501e8c114c7d400a1aea
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-04-03 14:10:24 -0300

    Create AndroidPortPlatform.cpp

---
+++ Assets/AndroidPortPlatform.cpp
@@ -0,0 +1,7 @@
+// this is the only godot-inspired part. Anything else will be custom. due to SDK reasons.
+#include "version.txt"
+#include ".ii"
+String get deafult android.sdk()
+String get deafult android.ndk()
+String get deafult android.Compile(binaryApkInstallable.Action)
+String get deafult CUDA(dlss="false")
